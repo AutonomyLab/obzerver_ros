ae2075b727d678388c8fde8390aff35c9677231f
Mani Monajjemi <mani.monajjemi@gmail.com>
2015-02-24 10:37:34 -0800

    Update ROS binding to support new Obzerver

--- src/obzerver_node.cpp
+++ src/obzerver_node.cpp
@@ -24,26 +24,37 @@
 #include "sensor_msgs/image_encodings.h"
 #include "sensor_msgs/RegionOfInterest.h"
 
-#include "glog/logging.h"
-
-#include "opencv2/features2d/features2d.hpp"
-
-#include "obzerver/logger.hpp"
-#include "obzerver/utility.hpp"
-#include "obzerver/benchmarker.hpp"
-#include "obzerver/camera_tracker.hpp"
-#include "obzerver/object_tracker.hpp"
-
-#include "obzerver_ros/object.h"
+#include "obzerver/periodicity_app.hpp"
+#include "obzerver_ros/Tracks.h"
 
 class ObzerverROS
 {
 public:
   template<class T>
-  static void GetParam(const ros::NodeHandle& nh, const std::string& param_name, T& var, const T& default_value)
+  static void GetParam(const ros::NodeHandle& nh,
+                       const std::string& param_name, T& var, const T& default_value)
   {
     nh.param<T>(param_name, var, default_value);
     ROS_INFO_STREAM("[OBR] Param " << param_name << " : " << var);
+  }
+
+  void CopyTracksToMsg(const std::vector<obz::Track>& tracks,
+                       obzerver_ros::Tracks& target_tracks_msg)
+  {
+    target_tracks_msg.tracks.resize(tracks.size());
+    for (std::size_t i = 0; i < tracks.size(); i++)
+    {
+      single_track_msg_.uid = tracks[i].uid;
+      single_track_msg_.roi.x_offset = tracks[i].GetBB().x;
+      single_track_msg_.roi.y_offset = tracks[i].GetBB().y;
+      single_track_msg_.roi.width = tracks[i].GetBB().width;
+      single_track_msg_.roi.height = tracks[i].GetBB().height;
+      single_track_msg_.status = obzerver_ros::Track::STATUS_TRACKING;
+      single_track_msg_.dominant_freq = tracks[i].dom_freq;
+      single_track_msg_.spectrum = tracks[i].avg_spectrum;
+      single_track_msg_.displacement = tracks[i].displacement;
+      target_tracks_msg.tracks[i] = single_track_msg_;
+    }
   }
 
 protected:
@@ -56,21 +67,12 @@ protected:
   image_transport::Publisher pub_stablized_image_;
   image_transport::Publisher pub_diff_image_;
   image_transport::Publisher pub_simmat_image_;
-  ros::Publisher pub_object_;
+  ros::Publisher pub_all_tracks_;
+  ros::Publisher pub_per_tracks_;
 
+  std::string param_cfg_file_;
   std::string param_log_file_;
-
   double param_downsample_factor_;
-  double param_fps_;
-
-  int param_max_features_;
-  int param_num_particles_;
-  int param_hist_len_;
-  int param_pylk_winsize_;
-  int param_pylk_iters_;
-  double param_pylk_eps_;
-  int param_ffd_threshold_;
-  int param_skip_dc_term_count_;
 
   bool enable_debug_image_;
   bool enable_stablized_image_;
@@ -78,7 +80,7 @@ protected:
   bool enable_simmat_image_;
 
   cv::Mat frame_input_;
-  cv::Mat frame_gray_;
+
   unsigned int last_seq_;
   sensor_msgs::ImageConstPtr cache_msg_;
   cv_bridge::CvImageConstPtr frame_input_cvptr_;
@@ -86,16 +88,16 @@ protected:
   cv_bridge::CvImage frame_sim_cvi_;
   cv_bridge::CvImage frame_stab_cvi_;
   cv_bridge::CvImage frame_diff_cvi_;
-  obzerver_ros::object out_object_;
 
-  bool do_convert_;
+  obzerver_ros::Track  single_track_msg_;
+  obzerver_ros::Tracks all_tracks_msg_;
+  obzerver_ros::Tracks per_tracks_msg_;
+
   bool do_downsample_;
-  cv::Ptr<cv::FeatureDetector> feature_detector_;
-  cv::Ptr<CameraTracker> camera_tracker_;
-  cv::Ptr<ObjectTracker> object_tracker_;
   std::size_t frame_counter_;
 
-  StepBenchmarker& ticker_;
+  obz::app::PeriodicityApp papp;
+  std::vector<obz::Track> tracks;
 
   void UpdateParams()
   {
@@ -104,179 +106,102 @@ protected:
     GetParam<bool>(private_nh_, "enable_diff_image", enable_diff_image_, false);
     GetParam<bool>(private_nh_, "enable_simmat_image", enable_simmat_image_, false);
 
+    GetParam<std::string>(private_nh_, "obz_configfile", param_cfg_file_, std::string(""));
     GetParam<std::string>(private_nh_, "obz_logfile", param_log_file_, std::string(""));
     GetParam<double>(private_nh_, "downsample_factor", param_downsample_factor_, 1.0);
-    GetParam<double>(private_nh_, "fps", param_fps_, 30.0);
-    GetParam<int>(private_nh_, "max_features", param_max_features_, 300);
-    GetParam<int>(private_nh_, "num_particles", param_num_particles_, 1000);
-    GetParam<int>(private_nh_, "history_length", param_hist_len_, 90);
-    GetParam<int>(private_nh_, "pylk_winsize", param_pylk_winsize_, 30);
-    GetParam<int>(private_nh_, "pylk_iterations", param_pylk_iters_, 30);
-    GetParam<double>(private_nh_, "pylk_eps", param_pylk_eps_, 0.01);
-    GetParam<int>(private_nh_, "ffd_threshold", param_ffd_threshold_, 30);
-    GetParam<int>(private_nh_, "skip_dc_term_count", param_skip_dc_term_count_, 1);
   }
 
   void Process()
   {
-    ticker_.reset();
+    StepBenchmarker::GetInstance().reset();
 
     // Do nothing when there is no new frame
     if (!cache_msg_ || last_seq_ == cache_msg_->header.seq) return;
     last_seq_ = cache_msg_->header.seq;
 
-    float _f = -1.0;
     try
     {
       if (sensor_msgs::image_encodings::isColor(cache_msg_->encoding))
       {
         ROS_WARN_ONCE("[OBR] Input image is BGR8");
-        frame_input_cvptr_ = cv_bridge::toCvShare(cache_msg_, sensor_msgs::image_encodings::BGR8);
-        do_convert_ = true;
+        frame_input_cvptr_ = cv_bridge::toCvShare(
+              cache_msg_, sensor_msgs::image_encodings::BGR8);
       }
       else
       {
         ROS_WARN_ONCE("[OBR] Input image is MONO8");
         frame_input_cvptr_ = cv_bridge::toCvShare(cache_msg_, sensor_msgs::image_encodings::MONO8);
-        do_convert_ = false;
       }
 
       frame_input_ = frame_input_cvptr_->image;  // No Copy For Now
-      ticker_.tick("Frame Copy");
+      TICK("ML_Frame_Copy");
       if (do_downsample_)
       {
         cv::resize(frame_input_, frame_input_, cv::Size(0, 0),
                    param_downsample_factor_, param_downsample_factor_, cv::INTER_CUBIC);
-        ticker_.tick("Downsampling.");
-      }
-
-      frame_gray_ = frame_input_;  // No Copy for now
-      if (do_convert_)
-      {
-        cv::cvtColor(frame_input_, frame_gray_, cv::COLOR_BGR2GRAY);
-        ticker_.tick("Frame 2 Gray");
+        TICK("ML_Downsampling");
       }
 
       LOG(INFO) << "Frame: " << frame_counter_ << " [" << frame_input_.cols << " x " << frame_input_.rows << "]";
 
-      const bool ct_success = camera_tracker_->Update(frame_gray_, frame_input_);
+      papp.Update(frame_input_);
 
-      out_object_.header.stamp = ros::Time::now();
-      out_object_.header.frame_id = frame_input_cvptr_->header.frame_id;
-      out_object_.status = 0;
-      if (!ct_success)
-      {
-        LOG(WARNING) << "Camera Tracker Failed";
-        // TODO(mani-monaj)
-      }
-      else
-      {
-        object_tracker_->Update(camera_tracker_->GetStablizedGray(),
-                               camera_tracker_->GetLatestDiff(),
-                               camera_tracker_->GetLatestSOF(),
-                               camera_tracker_->GetLatestCameraTransform());
+      all_tracks_msg_.header.stamp = ros::Time::now();
+      all_tracks_msg_.header.frame_id = frame_input_cvptr_->header.frame_id;
+      all_tracks_msg_.max_width = frame_input_cvptr_->image.cols;
+      all_tracks_msg_.max_height = frame_input_cvptr_->image.rows;
+      per_tracks_msg_.header.stamp = ros::Time::now();
+      per_tracks_msg_.header.frame_id = frame_input_cvptr_->header.frame_id;
+      per_tracks_msg_.max_width = frame_input_cvptr_->image.cols;
+      per_tracks_msg_.max_height = frame_input_cvptr_->image.rows;
 
-        out_object_.status = object_tracker_->GetStatus();
+      // tracks will be cleared
+      papp.GetMOTPtr()->CopyAllTracks(tracks);
+      CopyTracksToMsg(tracks, all_tracks_msg_);
+      ROS_ASSERT(tracks.size() == all_tracks_msg_.tracks.size());
 
-        LOG(INFO) << "Tracking status: " << object_tracker_->GetStatus();
-        if (object_tracker_->IsTracking())
-        {
-           // TODO(mani-monaj)
-          _f = object_tracker_->GetObject().GetPeriodicity().GetDominantFrequency(param_skip_dc_term_count_);
-          LOG(INFO) << "Object: "
-                    << object_tracker_->GetObjectBoundingBox()
-                    << " Periodicity:"
-                    << _f;
-          out_object_.roi.x_offset = object_tracker_->GetObjectBoundingBox().tl().x;
-          out_object_.roi.y_offset = object_tracker_->GetObjectBoundingBox().tl().y;
-          out_object_.roi.width = object_tracker_->GetObjectBoundingBox().width;
-          out_object_.roi.height = object_tracker_->GetObjectBoundingBox().height;
-          out_object_.max_width = frame_input_.cols;
-          out_object_.max_height = frame_input_.rows;
-          out_object_.spectrum = object_tracker_->GetObject().GetPeriodicity().GetSpectrum();
-          out_object_.dominant_freq = _f;
-          out_object_.displacement = 0.0;  // TODO(mani-monaj)
-        }
+      // tracks will be cleared
+      papp.GetPeriodicTracks(tracks);
+      CopyTracksToMsg(tracks, per_tracks_msg_);
+      ROS_ASSERT(tracks.size() == per_tracks_msg_.tracks.size());
 
-        // Publish
+      ROS_DEBUG_STREAM_THROTTLE(1, "All tracks: " << all_tracks_msg_.tracks.size()
+                               << " Periodic Tracks: " << per_tracks_msg_.tracks.size());
 
-        if (pub_object_.getNumSubscribers() > 0)
-        {
-          pub_object_.publish(out_object_);
-        }
+      // Publish
+      pub_all_tracks_.publish(all_tracks_msg_);
+      pub_per_tracks_.publish(per_tracks_msg_);
 
-        if (
+      if (
           enable_stablized_image_ &&
           pub_stablized_image_.getNumSubscribers() > 0 &&
-          camera_tracker_->GetStablizedGray().data
-        )
-        {
-          frame_stab_cvi_.image = camera_tracker_->GetStablizedGray();
-          frame_stab_cvi_.header.frame_id = frame_input_cvptr_->header.frame_id;
-          frame_stab_cvi_.header.stamp = frame_input_cvptr_->header.stamp;
-          frame_stab_cvi_.encoding = "mono8";
-          object_tracker_->DrawParticles(frame_stab_cvi_.image);
-          pub_stablized_image_.publish(frame_stab_cvi_.toImageMsg());
-        }
+          papp.GetCTCstPtr()->GetStablizedRGB().data
+          )
+      {
+        frame_stab_cvi_.image = papp.GetCTCstPtr()->GetStablizedRGB();
+        frame_stab_cvi_.header.frame_id = frame_input_cvptr_->header.frame_id;
+        frame_stab_cvi_.header.stamp = frame_input_cvptr_->header.stamp;
+        frame_stab_cvi_.encoding = "bgr8";
+        papp.GetMOTPtr()->DrawTracks(frame_stab_cvi_.image);
+        pub_stablized_image_.publish(frame_stab_cvi_.toImageMsg());
+      }
 
-        if (
-          enable_debug_image_ &&
-          pub_debug_image_.getNumSubscribers() > 0 &&
-          frame_input_.data
-        )
-        {
-          frame_debug_cvi_.image = frame_input_;
-          frame_debug_cvi_.header.frame_id = frame_input_cvptr_->header.frame_id;
-          frame_debug_cvi_.header.stamp = frame_input_cvptr_->header.stamp;
-          frame_debug_cvi_.encoding = do_convert_ ? "bgr8" : "mono8";
-
-          drawFeaturePointsTrajectory(frame_debug_cvi_.image,
-                                      camera_tracker_->GetHomographyOutliers(),
-                                      camera_tracker_->GetTrackedFeaturesPrev(),
-                                      camera_tracker_->GetTrackedFeaturesCurr(),
-                                      2,
-                                      cv::Scalar(127, 127, 127),
-                                      cv::Scalar(0, 0, 255),
-                                      cv::Scalar(0, 0, 255));
-
-          std::stringstream ss;
-          ss << std::setprecision(5) << "Periodicity: " << _f;
-          cv::putText(frame_debug_cvi_.image, ss.str(), cv::Point(40, 40), 1,
-                      CV_FONT_HERSHEY_PLAIN, cv::Scalar(0, 0, 255));
-
-          pub_debug_image_.publish(frame_debug_cvi_.toImageMsg());
-        }
-
-        if (
+      if (
           enable_diff_image_ &&
           pub_diff_image_.getNumSubscribers() > 0 &&
-          camera_tracker_->GetLatestDiff().data
-        )
-        {
-          frame_diff_cvi_.image = camera_tracker_->GetLatestDiff();
-          frame_diff_cvi_.header.frame_id = frame_input_cvptr_->header.frame_id;
-          frame_diff_cvi_.header.stamp = frame_input_cvptr_->header.stamp;
-          frame_diff_cvi_.encoding = "mono8";
-          pub_diff_image_.publish(frame_diff_cvi_.toImageMsg());
-        }
-
-        if (
-          enable_simmat_image_ &&
-          pub_simmat_image_.getNumSubscribers() > 0 &&
-          object_tracker_->IsTracking()
-        )
-        {
-          frame_sim_cvi_.image = object_tracker_->GetObject().GetSelfSimilarity().GetSimMatrixRendered();
-          frame_sim_cvi_.header.frame_id = frame_input_cvptr_->header.frame_id;
-          frame_sim_cvi_.header.stamp = frame_input_cvptr_->header.stamp;
-          frame_sim_cvi_.encoding = "mono8";
-          pub_simmat_image_.publish(frame_sim_cvi_.toImageMsg());
-        }
-
-        ticker_.tick("Visualization");
-        frame_counter_++;
-        LOG(INFO) << ticker_.getstr();
+          papp.GetCTCstPtr()->GetLatestDiff().data
+          )
+      {
+        frame_diff_cvi_.image = papp.GetCTCstPtr()->GetLatestDiff();
+        frame_diff_cvi_.header.frame_id = frame_input_cvptr_->header.frame_id;
+        frame_diff_cvi_.header.stamp = frame_input_cvptr_->header.stamp;
+        frame_diff_cvi_.encoding = "mono8";
+        pub_diff_image_.publish(frame_diff_cvi_.toImageMsg());
       }
+
+      TICK("Visualization");
+      frame_counter_++;
+      LOG(INFO) << StepBenchmarker::GetInstance().getstr();
     }
     catch (const cv_bridge::Exception& e)
     {
@@ -295,17 +220,24 @@ public:
     private_nh_("~"),
     ros_it_(ros_nh),
     last_seq_(-1),
-    frame_counter_(0),
-    ticker_(StepBenchmarker::GetInstance())
+    frame_counter_(0)
   {
     UpdateParams();
 
+    boost::program_options::variables_map vm;
+    if (!papp.Init(param_cfg_file_, param_log_file_, false, std::string(""), vm))
+    {
+      throw std::runtime_error("Error initializing obz::PeriodicityApp");
+    }
+
     sub_image_ = ros_it_.subscribe(
-                  "camera/image_raw",
-                  queue_size,
-                  &ObzerverROS::ImageCallback,
-                  this);
-    pub_object_ = ros_nh_.advertise<obzerver_ros::object>("obzerver/object", 20);
+          "camera/image_raw",
+          queue_size,
+          &ObzerverROS::ImageCallback,
+          this);
+
+    pub_all_tracks_ = ros_nh_.advertise<obzerver_ros::Tracks>("obzerver/tracks/all", 30);
+    pub_per_tracks_ = ros_nh_.advertise<obzerver_ros::Tracks>("obzerver/tracks/periodic", 30);
 
     if (enable_debug_image_)
     {
@@ -331,21 +263,14 @@ public:
       pub_simmat_image_ = ros_it_.advertise("obzerver/simmat_image", 1);
     }
 
-    obz_log_config("obzerver_ros_node", param_log_file_);
-
-    feature_detector_ = new cv::FastFeatureDetector(param_ffd_threshold_, true);
-    camera_tracker_ = new CameraTracker(param_hist_len_, feature_detector_, param_max_features_,
-                                       param_pylk_winsize_, param_pylk_iters_, param_pylk_eps_);
-    object_tracker_ = new ObjectTracker(param_num_particles_, param_hist_len_, param_fps_);
-
     do_downsample_ = param_downsample_factor_ < 1.0 && param_downsample_factor_ > 0.0;
   }
 
   virtual void spin()
   {
     // TODO(mani-monaj): Check if fixed freq. is better
-    ROS_INFO("[OBR] Setting ROS Loop Rate to %f hz", param_fps_);
-    ros::Rate rate(param_fps_);
+    //    ROS_INFO("[OBR] Setting ROS Loop Rate to %f hz", param_fps_);
+    ros::Rate rate(30.0);
     while (ros::ok())
     {
       spinOnce();
@@ -369,11 +294,26 @@ int main(int argc, char* argv[])
   ros::init(argc, argv, "obzerver_ros");
   ros::NodeHandle ros_nh;
   int param_queue_size;
-  ObzerverROS::GetParam<int>(ros_nh, "queue_size", param_queue_size, 1);
-  ObzerverROS obzerver_ros(param_queue_size, ros_nh);
 
-  /* Main Loop */
-  ROS_INFO("[OBR] Starting obzerver_ros");
-  obzerver_ros.spin();
+  ObzerverROS::GetParam<int>(ros_nh, "queue_size", param_queue_size, 1);
+
+
+  try
+  {
+    ObzerverROS obzerver_ros(param_queue_size, ros_nh);
+    obzerver_ros.spin();
+  }
+  catch (std::runtime_error& ex)
+  {
+    ROS_FATAL_STREAM("[OBZ] Runtime error: " << ex.what());
+    return 1;
+  }
+  catch (cv::Exception& ex)
+  {
+    ROS_FATAL_STREAM("[OBZ] OpenCV Exception: " << ex.what());
+    return 2;
+  }
+
+
   return 0;
 }
